32cc633940f56e71f1e971371f27ad46afa48615
megagig <megagigsoftwaresolutions@gmail.com>
2023-12-10 11:47:28 +0100

    Create Rails CRUD Scaffold and carried out migrations

---
+++ spec/views/friends/show.html.erb_spec.rb
@@ -0,0 +1,24 @@
+require 'rails_helper'
+
+RSpec.describe "friends/show", type: :view do
+  before(:each) do
+    assign(:friend, Friend.create!(
+      first_name: "First Name",
+      last_name: "Last Name",
+      email: "Email",
+      phone: "Phone",
+      twitter: "Twitter",
+      linkedin: "Linkedin"
+    ))
+  end
+
+  it "renders attributes in <p>" do
+    render
+    expect(rendered).to match(/First Name/)
+    expect(rendered).to match(/Last Name/)
+    expect(rendered).to match(/Email/)
+    expect(rendered).to match(/Phone/)
+    expect(rendered).to match(/Twitter/)
+    expect(rendered).to match(/Linkedin/)
+  end
+end
